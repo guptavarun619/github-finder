3bcf0a4dcb0ebc3316b073e47d9020f6d03d2d4b
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-13 19:53:31 +0530

    Added Clear button to remove searched github profile from the display

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,12 +6,19 @@ export class Search extends Component {
         text: ""
     };
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     };
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: "" });
+        if (this.state.text === "") {
+            this.props.setAlert("Please enter something", "light");
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: "" });
+        }
     };
 
     onChange = (e) => {
@@ -19,21 +26,32 @@ export class Search extends Component {
     };
 
     render() {
+        const { showClear, clearUsers } = this.props;
         return (
-            <form onSubmit={this.onSubmit} className="form">
-                <input
-                    type="text"
-                    name="text"
-                    placeholder="Search Users ..."
-                    value={this.state.text}
-                    onChange={this.onChange}
-                />
-                <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-dark btn-block"
-                />
-            </form>
+            <div>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Search Users ..."
+                        value={this.state.text}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        type="submit"
+                        value="Search"
+                        className="btn btn-dark btn-block"
+                    />
+                </form>
+                {showClear && (
+                    <button
+                        className="btn btn-light btn-block"
+                        onClick={clearUsers}
+                    >
+                        Clear
+                    </button>
+                )}
+            </div>
         );
     }
 }
