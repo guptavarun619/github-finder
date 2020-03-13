8d6ea0c66b4c1b2ac6f3de46714abb14b6706b41
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-13 13:51:34 +0530

    Implemented multiple users component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import "./App.css";
 
-class App extends Component{
+class App extends Component {
   render() {
     return (
       <div className="App">
         <nav className="navbar bg-primary">
           <Navbar></Navbar>
         </nav>
-        <UserItem></UserItem>
-      </div> 
+        <div className="container">
+          <Users></Users>
+        </div>
+      </div>
     );
   }
 }
