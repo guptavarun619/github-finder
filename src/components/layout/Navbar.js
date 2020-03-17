e9ecf1f94493e5bb3dc0ddb02b43149db112f542
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-17 23:52:08 +0530

    Implemented Context, created Reducer and Actions

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
     return (
-        <nav className="navbar bg-primary">
+        <nav className="navbar bg-dark ">
             <h1>
-                <i className={icon}></i> {title}
+                <Link to="/">
+                    <i className={icon}></i> {title}
+                </Link>
             </h1>
             <ul>
                 <li>
