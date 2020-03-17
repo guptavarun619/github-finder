fb940bb8548d61b3b0031be4f7a046ab280cf1e6
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-17 12:47:59 +0530

    Added individual user's page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -11,6 +12,8 @@ import "./App.css";
 class App extends Component {
     state = {
         users: [],
+        user: {},
+        repos: [],
         loading: false,
         alert: null
     };
@@ -30,10 +33,29 @@ class App extends Component {
         const res = await axios.get(
             `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
         );
-        console.log(res.data.item);
+        // console.log(res.data.item);
         this.setState({ users: res.data.items, loading: false });
     };
 
+    // Get a single Github User
+    getUser = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        // console.log(res.data.item);
+        this.setState({ user: res.data, loading: false });
+    };
+
+    // Get user's repo
+    getUserRepos = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        // console.log(res.data.item);
+        this.setState({ repos: res.data, loading: false });
+    };
     // Clear users from state
     clearUsers = () => {
         this.setState({ users: [], loading: false });
@@ -47,7 +69,7 @@ class App extends Component {
     };
 
     render() {
-        const { users, loading } = this.state;
+        const { user, users, repos, loading } = this.state;
         return (
             <Router>
                 <div className="App">
@@ -76,6 +98,20 @@ class App extends Component {
                                 )}
                             />
                             <Route exact path="/about" component={About} />
+                            <Route
+                                exact
+                                path="/user/:login"
+                                render={(props) => (
+                                    <User
+                                        {...props}
+                                        getUser={this.getUser}
+                                        getUserRepos={this.getUserRepos}
+                                        user={user}
+                                        repos={repos}
+                                        loading={loading}
+                                    />
+                                )}
+                            />
                         </Switch>
                     </div>
                 </div>
